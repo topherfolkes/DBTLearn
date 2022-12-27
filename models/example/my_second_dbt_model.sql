29fda15477aca2d15ec179cb3657ab7f19d7c0f8
cfolkes@geico.com <cfolkes@geico.com>
2022-12-27 21:25:31 +0000

    Revisions for testing

--- models/example/my_second_dbt_model.sql
+++ models/example/my_second_dbt_model.sql
@@ -1,6 +1,7 @@
 
--- Use the `ref` function to select from other models
+-- Use the `ref` function to select from other models , I see
 
 select *
 from {{ ref('my_first_dbt_model') }}
 where id = 1
+
