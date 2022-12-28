ee2b2b4566c480643613b4ea6ef8eebdd94f18ef
cfolkes@geico.com <cfolkes@geico.com>
2022-12-28 02:26:15 +0000

    Added additional func

--- models/marts/core/fct_orders.sql
+++ models/marts/core/fct_orders.sql
@@ -1,28 +1,30 @@
-with 
- customers as (
-
-    select * from {{ref ('stg_customers')}}
+with orders as  (
+    select * from {{ ref('stg_orders' )}}
 ),
-orders as (
 
-    select * from {{ref ('stg_orders')}}
-),
 payments as (
-    select * from {{ ref ('stg_payments')}}
+    select * from {{ ref('stg_payments') }}
+),
+
+order_payments as (
+    select
+        order_id,
+        sum(case when status = 'success' then amount end) as amount
+
+    from payments
+    group by 1
 ),
 
 final as (
 
     select
-        order_id,
-        customer_id,
-        customers.first_name,
-        amount
+        orders.order_id,
+        orders.customer_id,
+        orders.order_date,
+        coalesce(order_payments.amount, 0) as amount
 
-    from customers
-
-    left join orders using (customer_id) 
-    left join payments on payments.orderid = orders.order_id
+    from orders
+    left join order_payments using (order_id)
 )
 
 select * from final
